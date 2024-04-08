8b40b6dbe0dc715395e6778188e47be40f59a8e4
ro34 <ro34abc@gmail.com>
2024-04-09 00:43:04 +0800

    darknet_2_depthwise_separable_conv

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     #   Unfreeze_batch_size     模型在解冻后的batch_size
     #------------------------------------------------------------------#
     UnFreeze_Epoch      = 30
-    Unfreeze_batch_size = 16
+    Unfreeze_batch_size = 8
     #------------------------------------------------------------------#
     #   Freeze_Train    是否进行冻结训练
     #                   默认先冻结主干训练后解冻训练。
@@ -275,24 +275,38 @@ if __name__ == "__main__":
         #------------------------------------------------------#
         #   根据预训练权重的Key和模型的Key进行加载
         #------------------------------------------------------#
-        model_dict      = model.state_dict()
-        pretrained_dict = torch.load(model_path, map_location = device)
-        load_key, no_load_key, temp_dict = [], [], {}
+        model_dict = model.state_dict()
+        pretrained_dict = torch.load(model_path, map_location=device)
+        a = {}
         for k, v in pretrained_dict.items():
-            if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
-                temp_dict[k] = v
-                load_key.append(k)
-            else:
-                no_load_key.append(k)
-        model_dict.update(temp_dict)
+            try:    
+                if np.shape(model_dict[k]) ==  np.shape(v):
+                    a[k]=v
+            except:
+                pass
+        model_dict.update(a)
         model.load_state_dict(model_dict)
+        print('Finished!')
+        
+
+        # model_dict      = model.state_dict()
+        # pretrained_dict = torch.load(model_path, map_location = device)
+        # load_key, no_load_key, temp_dict = [], [], {}
+        # for k, v in pretrained_dict.items():
+        #     if k in model_dict.keys() and np.shape(model_dict[k]) == np.shape(v):
+        #         temp_dict[k] = v
+        #         load_key.append(k)
+        #     else:
+        #         no_load_key.append(k)
+        # model_dict.update(temp_dict)
+        # model.load_state_dict(model_dict)
         #------------------------------------------------------#
         #   显示没有匹配上的Key
         #------------------------------------------------------#
-        if local_rank == 0:
-            print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
-            print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
-            print("\n\033[1;33;44m温馨提示，head部分没有载入是正常现象，Backbone部分没有载入是错误的。\033[0m")
+        # if local_rank == 0:
+        #     print("\nSuccessful Load Key:", str(load_key)[:500], "……\nSuccessful Load Key Num:", len(load_key))
+        #     print("\nFail To Load Key:", str(no_load_key)[:500], "……\nFail To Load Key num:", len(no_load_key))
+        #     print("\n\033[1;33;44m温馨提示，head部分没有载入是正常现象，Backbone部分没有载入是错误的。\033[0m")
 
     #----------------------#
     #   获得损失函数
